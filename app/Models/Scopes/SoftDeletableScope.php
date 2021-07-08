4e7aa466e04b0ebaae7400a1b1d6af707d764f97
Andrii Prykhodko <andriichello@gmail.com>
2021-07-08 17:53:45 +0300

    Load customer and creater of banquet even if they are soft deleted.

--- app/Models/Scopes/SoftDeletableScope.php
+++ app/Models/Scopes/SoftDeletableScope.php
@@ -2,15 +2,24 @@
 
 namespace App\Models\Scopes;
 
+use App\Models\BanquetState;
+use App\Models\Customer;
+use App\Models\ImperiaUser;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use function Sodium\add;
 
 class SoftDeletableScope extends SoftDeletingScope
 {
     public function apply(Builder $builder, Model $model)
     {
+        if (str_contains(request()->getUri(), '/banquets')) {
+            if ($model instanceof Customer || $model instanceof ImperiaUser || $model instanceof BanquetState) {
+                $builder->withTrashed();
+                return;
+            }
+        }
+
         $trashed = request('trashed', 'without');
         if ($trashed === 'with') {
             $builder->withTrashed();
