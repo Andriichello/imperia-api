dd1589962c3d92961099bb5a2902412a8b347399
Andrii Prykhodko <andriichello@gmail.com>
2022-01-19 18:06:17 +0200

    Load migrations from database/migrations/imperia folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->loadMigrationsFrom('database/migrations/imperia');
     }
 
     /**
