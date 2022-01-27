c79517d2a385fe20f0f46bacaa32afd4116dee79
Andrii Prykhodko <andriichello@gmail.com>
2022-01-27 23:14:46 +0200

    Update exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
+/**
+ * Class Handler.
+ */
 class Handler extends ExceptionHandler
 {
     /**
@@ -25,9 +24,9 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontFlash = [
-        'api_token',
-        'current_password',
+        'token',
         'password',
+        'current_password',
         'password_confirmation',
     ];
 
@@ -38,31 +37,6 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        if (Route::getCurrentRoute() === null || str_ends_with(Route::getCurrentRoute()->getPrefix(), 'admin')) {
-            return;
-        }
-
-        $this->renderable(function (HttpException $httpException) {
-            return response([
-                'success' => false,
-                'message' => $httpException->getMessage(),
-            ], $httpException->getStatusCode(), $httpException->getHeaders());
-        });
-
-        $this->renderable(function (ValidationException $validationException) {
-            $statusCode = $validationException->status;
-            if (!in_array($statusCode, array_keys(Response::$statusTexts))) {
-                $statusCode = 400; // bad request
-            }
-
-            $message = $validationException->getMessage() ?? Response::$statusTexts[$statusCode];
-            $errors = $validationException->errors();
-
-            return response([
-                'success' => false,
-                'message' => $message,
-                'errors' => $errors,
-            ], $statusCode);
-        });
+        //
     }
 }
