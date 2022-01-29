c0860e36559d678667cc6caeede1458049fe113e
Andrii Prykhodko <andriichello@gmail.com>
2022-01-29 21:41:13 +0200

    Refactor commentable trait

--- app/Models/Traits/CommentableTrait.php
+++ app/Models/Traits/CommentableTrait.php
@@ -5,31 +5,92 @@ namespace App\Models\Traits;
 use App\Models\BaseModel;
 use App\Models\Morphs\Comment;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Trait CommentableTrait.
  *
  * @mixin BaseModel
+ *
+ * @property Comment[]|Collection $targetComments
+ * @property Comment[]|Collection $containerComments
  */
 trait CommentableTrait
 {
     /**
-     * Get all comments for model as a target.
+     * Comments related to the model.
      *
      * @return MorphMany
      */
-    public function targetComments(): MorphMany
+    public function comments(): MorphMany
     {
-        return $this->morphMany(Comment::class, 'target', 'target_type', 'target_id', 'id');
+        return $this->morphMany(Comment::class, 'commentable');
     }
 
     /**
-     * Get all comments for model as a container.
+     * Attach given comments to the model.
      *
-     * @return MorphMany
+     * @param string ...$texts
+     *
+     * @return void
      */
-    public function containerComments(): MorphMany
+    public function attachComments(string ...$texts): void
     {
-        return $this->morphMany(Comment::class, 'container', 'container_type', 'container_id', 'id');
+        DB::transaction(function () use ($texts) {
+            foreach ($texts as $text) {
+                Comment::query()
+                    ->create([
+                        'text' => $text,
+                        'commentable_id' => $this->id,
+                        'commentable_type' => $this->type,
+                    ]);
+            }
+        });
+    }
+
+    /**
+     * Detach given comments from the model.
+     *
+     * @param string ...$texts
+     *
+     * @return void
+     */
+    public function detachComments(string ...$texts): void
+    {
+        DB::transaction(function () use ($texts) {
+            Comment::query()
+                ->where('commentable_id', $this->id)
+                ->where('commentable_type', $this->type)
+                ->whereIn('text', $texts)
+                ->delete();
+        });
+    }
+
+    /**
+     * Determines if model has all comments attached.
+     *
+     * @param string ...$texts
+     *
+     * @return bool
+     */
+    public function hasAllOfComments(string ...$texts): bool
+    {
+        $texts = array_unique($texts);
+        $count = $this->comments()->whereIn('text', $texts)->count();
+        return count($texts) <= $count;
+    }
+
+    /**
+     * Determines if model has any of comments attached.
+     *
+     * @param string ...$texts
+     *
+     * @return bool
+     */
+    public function hasAnyOfComments(string ...$texts): bool
+    {
+        $texts = array_unique($texts);
+        return empty($texts) || $this->comments()->whereIn('text', $texts)->exists();
     }
 }
