bba993228fe20ad07895367674e4ee9c4ec80a67
Andrii Prykhodko <andriichello@gmail.com>
2022-01-29 21:42:14 +0200

    Create commentable trait test

---
+++ tests/Models/Traits/CommentableTraitTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Tests\Models\Traits;
+
+use App\Models\BaseModel;
+use App\Models\Morphs\Categorizable;
+use App\Models\Morphs\Category;
+use App\Models\Morphs\Comment;
+use App\Models\Traits\CategorizableTrait;
+use App\Models\Traits\CommentableTrait;
+use Tests\Models\StubModel;
+use Tests\StubsTestCase;
+
+/**
+ * Class CommentableTraitTest.
+ */
+class CommentableTraitTest extends StubsTestCase
+{
+    /**
+     * Instance of the tested class.
+     *
+     * @var BaseModel
+     */
+    protected BaseModel $instance;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->instance = new class extends StubModel {
+            use CommentableTrait;
+        };
+        $this->instance->save();
+    }
+
+    /**
+     * Test attachCategories and detachCategories methods.
+     *
+     * @return void
+     */
+    public function testAttachAndDetachCategories()
+    {
+        $texts = ['one', 'two'];
+
+        $this->instance->attachComments($texts[0]);
+        $this->assertTrue($this->instance->hasAllOfComments($texts[0]));
+        $this->assertEquals(1, $this->instance->comments()->count());
+
+        $this->instance->attachComments($texts[1]);
+        $this->assertTrue($this->instance->hasAllOfComments(...$texts));
+        $this->assertEquals(2, $this->instance->comments()->count());
+
+        $this->instance->detachComments($texts[0]);
+        $this->assertTrue($this->instance->hasAllOfComments($texts[1]));
+        $this->assertEquals(1, $this->instance->comments()->count());
+
+        $this->instance->detachComments($texts[1]);
+        $this->assertFalse($this->instance->hasAnyOfComments(...$texts));
+        $this->assertEquals(0, $this->instance->comments()->count());
+    }
+
+    /**
+     * Test hasAllOfComments method.
+     *
+     * @return void
+     */
+    public function testHasAllComments()
+    {
+        $texts = ['one', 'two'];
+
+        $this->assertTrue($this->instance->hasAllOfComments());
+        $this->assertFalse($this->instance->hasAllOfComments($texts[0]));
+        $this->assertFalse($this->instance->hasAllOfComments($texts[1]));
+        $this->assertFalse($this->instance->hasAllOfComments(...$texts));
+
+        Comment::factory()->withText($texts[0])->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAllOfComments());
+        $this->assertTrue($this->instance->hasAllOfComments($texts[0]));
+        $this->assertFalse($this->instance->hasAllOfComments($texts[1]));
+        $this->assertFalse($this->instance->hasAllOfComments(...$texts));
+
+        Comment::factory()->withText($texts[1])->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAllOfComments());
+        $this->assertTrue($this->instance->hasAllOfComments($texts[0]));
+        $this->assertTrue($this->instance->hasAllOfComments($texts[1]));
+        $this->assertTrue($this->instance->hasAllOfComments(...$texts));
+    }
+
+    /**
+     * Test hasAnyOfComments method.
+     *
+     * @return void
+     */
+    public function testHasAnyComments()
+    {
+        $texts = ['one', 'two'];
+
+        $this->assertTrue($this->instance->hasAnyOfComments());
+        $this->assertFalse($this->instance->hasAnyOfComments($texts[0]));
+        $this->assertFalse($this->instance->hasAnyOfComments($texts[1]));
+        $this->assertFalse($this->instance->hasAnyOfComments(...$texts));
+
+        Comment::factory()->withText($texts[0])->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAnyOfComments());
+        $this->assertTrue($this->instance->hasAnyOfComments($texts[0]));
+        $this->assertFalse($this->instance->hasAnyOfComments($texts[1]));
+        $this->assertTrue ($this->instance->hasAnyOfComments(...$texts));
+
+        Comment::factory()->withText($texts[1])->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAnyOfComments());
+        $this->assertTrue($this->instance->hasAnyOfComments($texts[0]));
+        $this->assertTrue($this->instance->hasAnyOfComments($texts[1]));
+        $this->assertTrue($this->instance->hasAnyOfComments(...$texts));
+    }
+}
