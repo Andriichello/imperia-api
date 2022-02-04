354afbc54480a9859673903999d2d0d907ed066d
Andrii Prykhodko <andriichello@gmail.com>
2022-02-04 17:44:09 +0200

    Create period and periodical tests

---
+++ tests/Models/Traits/PeriodicalTraitTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Models\Traits;
+
+use App\Models\Morphs\Periodical;
+use App\Models\Morphs\Period;
+use Tests\Models\Stubs\PeriodicalStub;
+use Tests\StubsTestCase;
+
+/**
+ * Class PeriodicalTraitTest.
+ */
+class PeriodicalTraitTest extends StubsTestCase
+{
+    /**
+     * Instance of the tested class.
+     *
+     * @var PeriodicalStub
+     */
+    protected PeriodicalStub $instance;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->instance = new PeriodicalStub();
+        $this->instance->save();
+    }
+
+    /**
+     * Test attachPeriods and detachPeriods methods.
+     *
+     * @return void
+     */
+    public function testAttachAndDetachPeriods()
+    {
+        $categoryOne = Period::factory()->create(['title' => 'one']);
+        $categoryTwo = Period::factory()->create(['title' => 'two']);
+
+        $this->instance->attachPeriods($categoryOne);
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryOne));
+        $this->assertEquals(1, $this->instance->periods()->count());
+
+        $this->instance->attachPeriods($categoryTwo);
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryOne, $categoryTwo));
+        $this->assertEquals(2, $this->instance->periods()->count());
+
+        $this->instance->detachPeriods($categoryOne);
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryTwo));
+        $this->assertEquals(1, $this->instance->periods()->count());
+
+        $this->instance->detachPeriods($categoryTwo);
+        $this->assertFalse($this->instance->hasAnyOfPeriods($categoryOne, $categoryTwo));
+        $this->assertEquals(0, $this->instance->periods()->count());
+    }
+
+    /**
+     * Test hasAllOfPeriods method.
+     *
+     * @return void
+     */
+    public function testHasAllPeriods()
+    {
+        $categoryOne = Period::factory()->create(['title' => 'one']);
+        $categoryTwo = Period::factory()->create(['title' => 'two']);
+
+        $this->assertTrue($this->instance->hasAllOfPeriods());
+        $this->assertFalse($this->instance->hasAllOfPeriods($categoryOne));
+        $this->assertFalse($this->instance->hasAllOfPeriods($categoryTwo));
+        $this->assertFalse($this->instance->hasAllOfPeriods($categoryOne, $categoryTwo));
+
+        Periodical::factory()->withPeriod($categoryOne)->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAllOfPeriods());
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryOne));
+        $this->assertFalse($this->instance->hasAllOfPeriods($categoryTwo));
+        $this->assertFalse($this->instance->hasAllOfPeriods($categoryOne, $categoryTwo));
+
+        Periodical::factory()->withPeriod($categoryTwo)->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAllOfPeriods());
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryOne));
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryTwo));
+        $this->assertTrue($this->instance->hasAllOfPeriods($categoryOne, $categoryTwo));
+    }
+
+    /**
+     * Test hasAnyOfPeriods method.
+     *
+     * @return void
+     */
+    public function testHasAnyPeriods()
+    {
+        $categoryOne = Period::factory()->create(['title' => 'one']);
+        $categoryTwo = Period::factory()->create(['title' => 'two']);
+
+        $this->assertTrue($this->instance->hasAnyOfPeriods());
+        $this->assertFalse($this->instance->hasAnyOfPeriods($categoryOne));
+        $this->assertFalse($this->instance->hasAnyOfPeriods($categoryTwo));
+        $this->assertFalse($this->instance->hasAnyOfPeriods($categoryOne, $categoryTwo));
+
+        Periodical::factory()->withPeriod($categoryOne)->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAnyOfPeriods());
+        $this->assertTrue($this->instance->hasAnyOfPeriods($categoryOne));
+        $this->assertFalse($this->instance->hasAnyOfPeriods($categoryTwo));
+        $this->assertTrue($this->instance->hasAnyOfPeriods($categoryOne, $categoryTwo));
+
+        Periodical::factory()->withPeriod($categoryTwo)->withModel($this->instance)->create();
+
+        $this->assertTrue($this->instance->hasAnyOfPeriods());
+        $this->assertTrue($this->instance->hasAnyOfPeriods($categoryOne));
+        $this->assertTrue($this->instance->hasAnyOfPeriods($categoryTwo));
+        $this->assertTrue($this->instance->hasAnyOfPeriods($categoryOne, $categoryTwo));
+    }
+}
