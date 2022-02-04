43a6d0c12c7f5557a9e8f34b083fb1bf434bd134
Andrii Prykhodko <andriichello@gmail.com>
2022-02-04 16:50:47 +0200

    Create discount calculation tests

--- tests/Models/Traits/DiscountableTraitTest.php
+++ tests/Models/Traits/DiscountableTraitTest.php
@@ -136,4 +136,61 @@ class DiscountableTraitTest extends StubsTestCase
         $this->assertTrue($this->instance->hasAnyOfDiscounts($discountTwo));
         $this->assertTrue($this->instance->hasAnyOfDiscounts($discountOne, $discountTwo));
     }
+
+    /**
+     * Test discount amount and percent.
+     *
+     * @return void
+     */
+    public function testDiscountAmountAndPercent()
+    {
+        $discountOne = Discount::factory()->create([
+            'title' => 'One',
+            'amount' => 10,
+            'percent' => 10,
+        ]);
+        $this->instance->attachDiscounts($discountOne);
+
+        $amount = $discountOne->amount;
+        $percent = $discountOne->percent;
+
+        $this->assertEquals($amount, $this->instance->discountsAmount());
+        $this->assertEquals($percent, $this->instance->discountsPercent());
+
+        $discountTwo = Discount::factory()->create([
+            'title' => 'Two',
+            'amount' => 20,
+            'percent' => 20,
+        ]);
+        $this->instance->attachDiscounts($discountTwo);
+
+        $amount = $discountOne->amount + $discountTwo->amount;
+        $percent = $discountOne->percent + $discountTwo->percent;
+
+        $this->assertEquals($amount, $this->instance->discountsAmount());
+        $this->assertEquals($percent, $this->instance->discountsPercent());
+    }
+
+    /**
+     * Test apply discounts.
+     *
+     * @return void
+     */
+    public function testApplyDiscounts()
+    {
+        $discount = Discount::factory()->create([
+            'title' => 'One',
+            'amount' => 10,
+            'percent' => 10,
+        ]);
+        $this->instance->attachDiscounts($discount);
+
+        $discounted = $this->instance->applyDiscounts($price = 100.0);
+        $calculated = $discount->amount + $price * $discount->percent / 100.0;
+        $this->assertEquals($discounted, max(0.0, $price - $calculated));
+
+        $discount->update(['amount' => $price]);
+        $discounted = $this->instance->applyDiscounts($price);
+        $this->assertEquals(0.0, $discounted);
+    }
 }
