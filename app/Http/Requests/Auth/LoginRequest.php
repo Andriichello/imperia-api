1416110e05d4fede5e0944ad25298edc866e2d8e
Andrii Prykhodko <andriichello@gmail.com>
2022-02-16 23:06:09 +0200

    Change login password example

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -83,7 +83,7 @@ class LoginRequest extends BaseRequest
     by remember token (`remember_token`).",
      *   @OA\Property(property="email", type="string", example="john.doe@email.com",
      *     description="By credentials. Required with `password`"),
-     *   @OA\Property(property="password", type="string", example="password",
+     *   @OA\Property(property="password", type="string", example="pa$$w0rd",
      *     description="By credentials. Required with `email`"),
      *   @OA\Property(property="remember_token", type="string",
      *     description="By remember token"))
