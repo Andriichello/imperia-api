744b1c18535efbbc0657dbdf364f77b298a77994
Andrii Prykhodko <andriichello@gmail.com>
2022-03-11 19:59:33 +0200

    Create method for getting model's media query

--- app/Models/Traits/MediableTrait.php
+++ app/Models/Traits/MediableTrait.php
@@ -3,9 +3,13 @@
 namespace App\Models\Traits;
 
 use App\Models\BaseModel;
+use App\Models\Morphs\Media;
+use App\Queries\MediaQueryBuilder;
 use ClassicO\NovaMediaLibrary\API as MediaLibraryAPI;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Laravel\Nova\Fields\HasMany;
 
 /**
  * Trait MediableTrait.
@@ -17,6 +21,26 @@ use Illuminate\Support\Collection;
  */
 trait MediableTrait
 {
+    /**
+     * Create query for related media.
+     *
+     * @return MediaQueryBuilder
+     */
+    public function media(): MediaQueryBuilder
+    {
+        return Media::query()->whereIn('id', $this->media_ids);
+    }
+
+    /**
+     * Accessor for the image.
+     *
+     * @return Collection
+     */
+    public function getMediaAttribute(): Collection
+    {
+        return $this->media()->get();
+    }
+
     /**
      * Accessor for the image ids property of metadata.
      *
@@ -35,16 +59,5 @@ trait MediableTrait
     public function setMediaIdsAttribute(mixed $value): void
     {
         $this->setToJson('metadata', 'media_ids', $value);
-    }
-
-    /**
-     * Accessor for the image.
-     *
-     * @return Collection
-     */
-    public function getMediaAttribute(): Collection
-    {
-        $media = MediaLibraryAPI::getFiles($this->media_ids, null, true);
-        return Collection::wrap($media);
     }
 }
