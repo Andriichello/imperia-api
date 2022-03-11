2c644b84460cb87fb68de2f3acc391f7c9faa64b
Andrii Prykhodko <andriichello@gmail.com>
2022-03-11 23:44:00 +0200

    Add default media to mediable

--- app/Models/Traits/MediableTrait.php
+++ app/Models/Traits/MediableTrait.php
@@ -4,9 +4,14 @@ namespace App\Models\Traits;
 
 use App\Models\BaseModel;
 use App\Models\Morphs\Media;
+use App\Models\Product;
+use App\Models\Service;
+use App\Models\Space;
+use App\Models\Ticket;
 use App\Queries\MediaQueryBuilder;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 /**
  * Trait MediableTrait.
@@ -15,11 +20,54 @@ use Illuminate\Support\Collection;
  *
  * @property array $media_ids
  * @property Collection $media
+ * @property Collection $default_media
  */
 trait MediableTrait
 {
     /**
-     * Create query for related media.
+     * Class name to default media relative path mappings.
+     *
+     * @var array
+     */
+    protected static array $defaultMediaMap = [
+        Space::class => 'defaults/table.svg',
+        Ticket::class => 'defaults/ticket.svg',
+        Product::class => 'defaults/dish.svg',
+        Service::class => 'defaults/action.svg',
+    ];
+
+    /**
+     * Default media collection.
+     *
+     * @var Collection
+     */
+    protected static Collection $defaultMedia;
+
+    /**
+     * Query for related default media.
+     *
+     * @return MediaQueryBuilder
+     */
+    public static function defaultMedia(): MediaQueryBuilder
+    {
+        $parts = Str::of(data_get(static::$defaultMediaMap, static::class))
+            ->explode('/')
+            ->filter();
+
+        return Media::query()->where('name', $parts->pop())
+            ->fromFolder($parts->implode('/'));
+    }
+
+    /**
+     * @return Collection
+     */
+    public static function getDefaultMediaAttribute(): Collection
+    {
+        return static::$defaultMedia = static::$defaultMedia ?? static::defaultMedia()->get();
+    }
+
+    /**
+     * Query for related media.
      *
      * @return MediaQueryBuilder
      */
@@ -42,13 +90,5 @@ trait MediableTrait
     public function getMediaIdsAttribute(): array
     {
         return Arr::wrap($this->getFromJson('metadata', 'media_ids'));
-    }
-
-    /**
-     * @param mixed $value
-     */
-    public function setMediaIdsAttribute(mixed $value): void
-    {
-        $this->setToJson('metadata', 'media_ids', $value);
     }
 }
