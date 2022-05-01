5d355ba602fba67325edaf448e3a3b43c8c1c686
Andrii Prykhodko <andriichello@gmail.com>
2022-05-01 22:31:17 +0300

    Create migration for notifications table creation

---
+++ database/migrations/2022_05_01_182114_create_notifications_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender_id')->nullable();
+            $table->unsignedBigInteger('receiver_id');
+            $table->string('channel');
+            $table->text('data')->nullable();
+            $table->timestamp('send_at');
+            $table->timestamp('sent_at')->nullable();
+
+            $table->foreign('sender_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->foreign('receiver_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
