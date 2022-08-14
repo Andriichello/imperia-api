232f09d208cc8090edb7b09def778967c2c690ec
Andrii Prykhodko <andriichello@gmail.com>
2022-08-14 20:51:23 +0300

    Create restaurants table

---
+++ database/migrations/2022_08_14_172422_create_restaurants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('name');
+            $table->string('country');
+            $table->string('city');
+            $table->string('place');
+            $table->text('metadata')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
