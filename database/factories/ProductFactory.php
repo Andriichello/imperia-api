cbb7557ac4320c6529c5408436b2726482a68b1a
Andrii Prykhodko <andriichello@gmail.com>
2022-09-15 22:07:08 +0300

    Update Holiday and Product factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -34,24 +34,6 @@ class ProductFactory extends Factory
             'description' => $this->faker->sentence(5),
             'price' => $this->faker->randomFloat(2, 10, 100),
             'weight' => $this->faker->randomFloat(2, 100, 1000),
-            'menu_id' => Menu::factory(),
         ];
-    }
-
-    /**
-     * Indicate related menu.
-     *
-     * @param Menu $menu
-     *
-     * @return static
-     */
-    public function withMenu(Menu $menu): static
-    {
-        return $this->state(
-            function (array $attributes) use ($menu) {
-                $attributes['menu_id'] = $menu->id;
-                return $attributes;
-            }
-        );
     }
 }
