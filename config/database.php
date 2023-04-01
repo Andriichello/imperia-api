c72778b7ba62b47ce7568692a0c35798a6b89da6
Andrii Prykhodko <andriichello@gmail.com>
2023-04-01 13:21:39 +0300

    Update PHPUnit configs

--- config/database.php
+++ config/database.php
@@ -73,7 +73,9 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => true,
+            'engine' => 'InnoDB',
         ],
 
         'pgsql' => [
