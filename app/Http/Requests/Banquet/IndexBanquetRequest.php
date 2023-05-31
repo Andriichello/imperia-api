3a3ef4254c97483c88c59ab2259882cecb91934d
Andrii Prykhodko <andriichello@gmail.com>
2023-05-31 08:42:23 +0300

    Add banquet filters such as from, until and search

--- app/Http/Requests/Banquet/IndexBanquetRequest.php
+++ app/Http/Requests/Banquet/IndexBanquetRequest.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Requests\Banquet;
 
+use App\Http\Filters\BanquetsSearchFilter;
 use App\Http\Requests\Crud\IndexRequest;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Carbon;
 use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder as SpatieBuilder;
 
 /**
  * Class IndexBanquetRequest.
@@ -15,7 +20,12 @@ class IndexBanquetRequest extends IndexRequest
         return array_merge(
             parent::getAllowedFilters(),
             [
+                AllowedFilter::custom('search', new BanquetsSearchFilter()),
+                AllowedFilter::callback('from', fn() => null),
+                AllowedFilter::callback('until', fn() => null),
                 AllowedFilter::exact('restaurant_id'),
+                AllowedFilter::exact('state'),
+
             ]
         );
     }
@@ -46,5 +56,39 @@ class IndexBanquetRequest extends IndexRequest
                 //
             ]
         );
+    }
+
+    /**
+     * Apply allowed options to spatie builder.
+     *
+     * @param Builder|EloquentBuilder|SpatieBuilder $builder
+     *
+     * @return SpatieBuilder
+     */
+    public function spatieBuilder(SpatieBuilder|EloquentBuilder|Builder $builder): SpatieBuilder
+    {
+        $builder = parent::spatieBuilder($builder);
+
+        $filters = $this->get('filter', []);
+
+        $from = data_get($filters, 'from');
+        $until = data_get($filters, 'until');
+
+        if ($from && !$until) {
+            $builder->where('start_at', '>=', Carbon::make($from)->setTime(0, 0));
+        }
+
+        if (!$from && $until) {
+            $builder->where('end_at', '<=', Carbon::make($until)->setTime(23, 59));
+        }
+
+        if ($from && $until) {
+            $builder->where(function (EloquentBuilder $q) use ($from, $until) {
+                $q->where('start_at', '>=', Carbon::make($from)->setTime(0, 0))
+                    ->where('end_at', '<=', Carbon::make($until)->setTime(23, 59));
+            });
+        }
+
+        return $builder;
     }
 }
