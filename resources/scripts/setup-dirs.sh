12870b5d1cd67b270b17be6cdd044c4377c41e54
Andrii Prykhodko <andriichello@gmail.com>
2023-06-09 10:17:17 +0300

    Only create folders and files when they are missing in setup-dirs.sh and setup-logs.sh

--- resources/scripts/setup-dirs.sh
+++ resources/scripts/setup-dirs.sh
@@ -1,4 +1,14 @@
-# Create directories.
-mkdir -p /var/log/
-mkdir -p /var/run/
-mkdir -p /var/run/php/
+# Create /var/log if it doesn't exist
+if [ ! -d "/var/log" ]; then
+    mkdir -p /var/log
+fi
+
+# Create /var/run if it doesn't exist
+if [ ! -d "/var/run" ]; then
+    mkdir -p /var/run
+fi
+
+# Create /var/run/php if it doesn't exist
+if [ ! -d "/var/run/php" ]; then
+    mkdir -p /var/run/php
+fi
