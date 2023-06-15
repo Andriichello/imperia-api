04a0bcf9941d41b07c30c0534439b032261dfc85
Andrii Prykhodko <andriichello@gmail.com>
2023-06-15 15:17:31 +0300

    Create migration for product_variants table creation

---
+++ database/migrations/2023_06_15_095950_create_product_variants_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('product_id');
+
+            $table->decimal('price')->unsigned();
+            $table->decimal('weight')->unsigned();
+            $table->string('weight_unit', 10);
+
+            $table->timestamps();
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
