9d959ae6445fce4df201be94fc8904e4fcc417b7
Andrii Prykhodko <andriichello@gmail.com>
2023-07-01 21:42:04 +0300

    Remove l5-swagger:generate command from start.sh

--- resources/docker/opt/start.sh
+++ resources/docker/opt/start.sh
@@ -13,7 +13,6 @@ cat .env
 php artisan storage:link
 php artisan cache:clear
 php artisan route:cache
-php artisan l5-swagger:generate
 
 # Create /var/run/php if it doesn't exist
 if [ ! -d "/var/run/php" ]; then
