193c37a8ad9421d8dccfeb2eb2b0c8fcb1f8e612
Andrii Prykhodko <andriichello@gmail.com>
2023-09-08 21:31:23 +0300

    Add restaurant_id to customer store and update requests

--- app/Http/Requests/Customer/StoreCustomerRequest.php
+++ app/Http/Requests/Customer/StoreCustomerRequest.php
@@ -3,9 +3,13 @@
 namespace App\Http\Requests\Customer;
 
 use App\Http\Requests\Crud\StoreRequest;
+use App\Models\Customer;
 use App\Models\Morphs\Comment;
+use App\Models\Restaurant;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
 use OpenApi\Annotations as OA;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 /**
  * Class StoreCustomerRequest.
@@ -23,6 +27,14 @@ class StoreCustomerRequest extends StoreRequest
             parent::rules(),
             Comment::rulesForAttaching(),
             [
+                'restaurant_id' => [
+                    'required',
+                    'integer',
+                    Rule::exists(
+                        Restaurant::class,
+                        'id'
+                    ),
+                ],
                 'name' => [
                     'required',
                     'string',
@@ -36,13 +48,11 @@ class StoreCustomerRequest extends StoreRequest
                 'email' => [
                     'required_without:phone',
                     'email',
-                    'unique:customers,email'
                 ],
                 'phone' => [
                     'required_without:email',
                     'nullable',
                     'regex:/(\+?[0-9]{1,2})?[0-9]{10,12}/',
-                    'unique:customers,phone'
                 ],
                 'birthdate' => [
                     'sometimes',
@@ -54,22 +64,70 @@ class StoreCustomerRequest extends StoreRequest
         );
     }
 
+    /**
+     * Get form request fields' default values.
+     *
+     * @return array
+     */
+    protected function defaults(): array
+    {
+        $user = $this->user();
+
+        $defaults = [];
+
+        if ($this->missing('restaurant_id')) {
+            $defaults['restaurant_id'] = $user->restaurant_id;
+        }
+
+        return $defaults;
+    }
+
     public function withValidator(Validator $validator)
     {
         $validator->after(function (Validator $validator) {
-            $email = $this->get('email');
-            $phone = $this->get('phone');
+            $user = $this->user();
+            $restaurantId = $this->get('restaurant_id');
 
-            if (!$email && !$phone) {
+            if ($user->restaurant_id && $user->restaurant_id !== $restaurantId) {
                 $validator->errors()
                     ->add(
-                        'email',
-                        'The email or phone must be specified.'
-                    )
-                    ->add(
-                        'phone',
-                        'The email or phone must be specified.'
+                        'restaurant_id',
+                        'The restaurant id must be the same as user\'s.'
                     );
+            }
+
+            $email = $this->get('email');
+
+            if ($email) {
+                $exists = Customer::query()
+                    ->where('email', $email)
+                    ->where('restaurant_id', $restaurantId)
+                    ->exists();
+
+                if ($exists) {
+                    $validator->errors()
+                        ->add(
+                            'email',
+                            "The email must be unique within restaurant (id: $restaurantId)"
+                        );
+                }
+            }
+
+            $phone = $this->get('phone');
+
+            if ($phone) {
+                $exists = Customer::query()
+                    ->where('phone', $phone)
+                    ->where('restaurant_id', $restaurantId)
+                    ->exists();
+
+                if ($exists) {
+                    $validator->errors()
+                        ->add(
+                            'phone',
+                            "The phone must be unique within restaurant (id: $restaurantId)"
+                        );
+                }
             }
         });
     }
@@ -79,7 +137,8 @@ class StoreCustomerRequest extends StoreRequest
      *   schema="StoreCustomerRequest",
      *   description="Store customer request. `email` or `phone`
            must be specified.",
-     *   required={"name", "surname", "email", "phone", "birthdate"},
+     *   required={"restaurant_id", "name", "surname", "email", "phone", "birthdate"},
+     *   @OA\Property(property="restaurant_id", type="integer", nullable="true", example=1),
      *   @OA\Property(property="name", type="string", example="John"),
      *   @OA\Property(property="surname", type="string", example="Forrester"),
      *   @OA\Property(property="email", type="string", nullable="true", example="ben.forrester@email.com"),
--- app/Http/Resources/Customer/CustomerResource.php
+++ app/Http/Resources/Customer/CustomerResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Customer;
 
 use App\Http\Resources\Comment\CommentCollection;
 use App\Http\Resources\FamilyMember\FamilyMemberCollection;
+use App\Http\Resources\Restaurant\RestaurantResource;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -28,12 +29,14 @@ class CustomerResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'restaurant_id' => $this->restaurant_id,
             'type' => $this->type,
             'name' => $this->name,
             'surname' => $this->surname,
             'email' => $this->email,
             'phone' => $this->phone,
             'birthdate' => $this->birthdate,
+            'restaurant' => new RestaurantResource($this->whenLoaded('restaurant')),
             'family_members' => new FamilyMemberCollection($this->whenLoaded('familyMembers')),
             'comments' => new CommentCollection($this->whenLoaded('comments')),
         ];
@@ -43,14 +46,16 @@ class CustomerResource extends JsonResource
      * @OA\Schema(
      *   schema="Customer",
      *   description="Customer resource object",
-     *   required = {"id", "type", "name", "surname", "email", "phone", "birthdate"},
+     *   required = {"id", "restaurant_id", "type", "name", "surname", "email", "phone", "birthdate"},
      *   @OA\Property(property="id", type="integer", example=1),
+     *   @OA\Property(property="restaurant_id", type="integer", nullable="true", example=1),
      *   @OA\Property(property="type", type="string", example="customers"),
      *   @OA\Property(property="name", type="string", example="John"),
      *   @OA\Property(property="surname", type="string", example="Doe"),
      *   @OA\Property(property="phone", type="string", example="+380507777777"),
      *   @OA\Property(property="email", type="string", nullable="true", example="john.doe@email.com"),
      *   @OA\Property(property="birthdate", type="string", nullable="true", format="date", example="1986-01-26"),
+     *   @OA\Property(property="restaurant", ref ="#/components/schemas/Restaurant"),
      *   @OA\Property(property="family_members", type="array",
      *     @OA\Items(ref ="#/components/schemas/FamilyMember")),
      *   @OA\Property(property="comments", type="array",
