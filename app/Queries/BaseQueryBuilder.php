0aa5e85560f02d429b1db96c1f5a4016aefd24ca
Andrii Prykhodko <andriichello@gmail.com>
2023-12-02 21:02:41 +0200

    Add within() method to BaseQueryBuilder

--- app/Queries/BaseQueryBuilder.php
+++ app/Queries/BaseQueryBuilder.php
@@ -4,8 +4,10 @@ namespace App\Queries;
 
 use App\Models\User;
 use App\Queries\Interfaces\IndexableInterface;
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder;
 
 /**
  * Class BaseQueryBuilder.
@@ -77,5 +79,34 @@ class BaseQueryBuilder extends EloquentBuilder implements IndexableInterface
     protected function extract(string $key, mixed ...$items): array
     {
         return extractValues($key, ...$items);
+    }
+
+    /**
+     * Only records within given dates.
+     *
+     * @param Carbon|null $beg
+     * @param Carbon|null $end
+     * @param string $column
+     *
+     * @return $this
+     */
+    public function within(?Carbon $beg, ?Carbon $end, string $column = 'created_at'): static
+    {
+        if (isset($beg) && empty($end)) {
+            $this->where($column, '>=', $beg);
+        }
+
+        if (empty($beg) && isset($end)) {
+            $this->where($column, '<=', $end);
+        }
+
+        if (isset($beg) && isset($end)) {
+            $this->whereNested(function (Builder $query) use ($beg, $end, $column) {
+                $query->where($column, '>=', $beg)
+                    ->where($column, '<=', $end);
+            });
+        }
+
+        return $this;
     }
 }
