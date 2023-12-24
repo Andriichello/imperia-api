1499b283d12067a2f44c669b9f282cd7685c7731
Andrii Prykhodko <andriichello@gmail.com>
2023-12-24 20:35:04 +0200

    Add methods for filtering by paths to CacheQueryBuilder

--- app/Queries/CacheQueryBuilder.php
+++ app/Queries/CacheQueryBuilder.php
@@ -69,4 +69,67 @@ class CacheQueryBuilder extends BaseQueryBuilder
 
         return $this;
     }
+
+    /**
+     * Filter to records, which belong to the given path.
+     *
+     * @param string $path
+     * @param string $boolean
+     *
+     * @return $this
+     */
+    public function withPath(string $path, string $boolean = 'and'): static
+    {
+        $this->where('key', 'like', "%[%]:<$path%>:{", boolean: $boolean);
+
+        return $this;
+    }
+
+    /**
+     * Filter to records, which belong to the given path.
+     *
+     * @param string $path
+     *
+     * @return $this
+     */
+    public function orWithPath(string $path): static
+    {
+        return $this->withPath($path, 'or');
+    }
+
+    /**
+     * Filter to records, which belong to all the given paths.
+     *
+     * @param string ...$paths
+     *
+     * @return $this
+     */
+    public function withAllOfPaths(string ...$paths): static
+    {
+        $this->whereWrapped(function (CacheQueryBuilder $query) use ($paths) {
+            foreach ($paths as $path) {
+                $query->withPath($path);
+            }
+        });
+
+        return $this;
+    }
+
+    /**
+     * Filter to records, which belong to any of the given groups.
+     *
+     * @param string ...$paths
+     *
+     * @return $this
+     */
+    public function withAnyOfPaths(string ...$paths): static
+    {
+        $this->whereWrapped(function (CacheQueryBuilder $query) use ($paths) {
+            foreach ($paths as $index => $path) {
+                $query->withPath($path, $index ? 'or' : 'and');
+            }
+        });
+
+        return $this;
+    }
 }
