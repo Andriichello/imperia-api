8939e101a905a584c1c6a05359d2abbe8acb5685
Andrii Prykhodko <andriichello@gmail.com>
2023-12-25 12:32:59 +0200

    Add migration for waiters table creation

---
+++ database/migrations/2023_12_25_091009_create_waiters_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('waiters', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('restaurant_id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('phone', 25)->nullable();
+            $table->string('email')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->text('about')->nullable();
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['restaurant_id', 'phone'], 'waiters_unique_PHONE');
+            $table->unique(['restaurant_id', 'email'], 'waiters_unique_EMAIL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('waiters');
+    }
+};
