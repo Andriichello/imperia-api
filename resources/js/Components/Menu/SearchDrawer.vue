40df4419e74b88359203cd12598eed76f2efb84a
Andrii Prykhodko <andriichello@gmail.com>
2025-05-24 23:16:56 +0300

    Fix issues with open menu and open category events

--- resources/js/Components/Menu/SearchDrawer.vue
+++ resources/js/Components/Menu/SearchDrawer.vue
@@ -39,9 +39,14 @@
   const emits = defineEmits(['close', 'open-menu', 'open-category', 'open-product', 'query-updated']);
 
   const searchInputRef = ref<HTMLInputElement | null>(null);
+  const hasResults = ref(false);
 
   function close() {
     emits('close');
+  }
+
+  function setHasResults(has: boolean) {
+    hasResults.value = has;
   }
 
   function openMenu(menu: Menu) {
@@ -49,11 +54,11 @@
     close();
   }
 
-  function openCategory(category: Category) {
-    const menu = props.menus?.filter(menu => menu.categories.includes(category))[0] ?? null;
+  function openCategory(category: Category, menu: Menu = null as any) {
+    const m = menu ?? props.menus?.filter(menu => menu.categories.includes(category))[0] ?? null;
 
-    if (menu) {
-      emits('open-category', category, menu);
+    if (m) {
+      emits('open-category', category, m);
       close();
     }
   }
@@ -87,10 +92,46 @@
                     :restaurant="restaurant"
                     :menus="menus"
                     :loading="loading"
-                    :withPlaceholders="true"
+                    :withPlaceholders="false"
+                    @close="close"
+                    @has-results-changed="setHasResults"
                     @open-menu="openMenu"
                     @open-category="openCategory"
                     @open-product="openProduct"
                     @query-updated="onQueryUpdated"/>
+
+    <div class="w-full h-full flex flex-col px-6 overflow-auto"
+         v-if="!hasResults">
+      <template v-for="menu in menus" :key="menu.id">
+        <div class="w-full flex flex-col text-start py-3 px-3 cursor-pointer"
+             @click="openMenu(menu)">
+          <h3 class="text-xl font-bold">
+            {{ menu.title }}
+          </h3>
+          <p class="text-md font-light opacity-80">
+            {{ menu.description?.length ? menu.description : 'menu' }}
+          </p>
+        </div>
+
+        <div class="w-full h-[1px] bg-base-300"/>
+
+        <div class="w-full flex flex-col pl-5">
+          <template v-for="category in menu.categories" :key="category.id">
+            <div class="w-full flex flex-col text-start py-3 px-3 cursor-pointer"
+                 @click="openCategory(category, menu)">
+              <h3 class="text-lg font-bold">
+                {{ category.title }}
+              </h3>
+              <p class="text-md font-light opacity-80"
+                 v-if="category!.description?.length">
+                {{ category.description }}
+              </p>
+            </div>
+          </template>
+        </div>
+
+        <div class="w-full h-[1px] bg-base-300"/>
+      </template>
+    </div>
   </BaseDrawer>
 </template>
