6510eae803bfa5ef598d51a3627fa6b73a08e7e5
Andrii Prykhodko <andriichello@gmail.com>
2025-09-06 10:58:32 +0300

    Prefix route key name with table name

--- app/Filament/BaseResource.php
+++ app/Filament/BaseResource.php
@@ -27,4 +27,22 @@ abstract class BaseResource extends Resource
         return $query->withoutGlobalScopes()
             ->index(request()->user());
     }
+
+    public static function getRecordRouteKeyName(): ?string
+    {
+        $name = parent::getRecordRouteKeyName();
+
+        $modelClass = static::getModel();
+        $model = new $modelClass();
+
+        // Default to the model's route key name if none is set on the resource
+        $name = $name ?: $model->getRouteKeyName();
+
+        // Qualify the column with the table name to avoid ambiguity when joins are present
+        if (!str_contains($name, '.')) {
+            $name = $model->getTable() . '.' . $name;
+        }
+
+        return $name;
+    }
 }
