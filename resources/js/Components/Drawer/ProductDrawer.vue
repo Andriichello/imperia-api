974914f218c4202c2959e63357bdafdae4ddc681
Andrii Prykhodko <andriichello@gmail.com>
2025-09-20 12:56:42 +0300

    Add image placeholder to ProductDrawer

--- resources/js/Components/Drawer/ProductDrawer.vue
+++ resources/js/Components/Drawer/ProductDrawer.vue
@@ -58,13 +58,13 @@ const closePopup = () => {
   <BaseDrawer :open="isOpen" @close="closePopup">
     <div class="w-full h-full flex flex-col overflow-auto -translate-y-12">
       <template v-if="product.media?.length">
-        <div class="w-full h-65 rounded-t relative">
-          <div class="absolute w-full top-0 h-65 border-b-1 border-base-300 overflow-hidden flex flex-col justify-center rounded-t">
+        <div class="w-full h-65 relative">
+          <div class="absolute w-full top-0 h-65 border-b-1 border-base-300 overflow-hidden flex flex-col justify-center">
             <DiagonalPattern class="scale-165 opacity-60 text-warning-content/80"
                              :establishment="establishment ?? 'restaurant'"/>
           </div>
 
-          <Splide class="w-full h-65 rounded-t" :options="{
+          <Splide class="w-full h-65" :options="{
                     perPage: 1,
                     perMove: 1,
                     rewind: false,
@@ -74,7 +74,7 @@ const closePopup = () => {
                     pagination: Number(media!.length) > 1,
                   }">
             <SplideSlide v-for="(m, index) in media" :key="m.id">
-              <img class="w-full h-65 object-cover object-center rounded-t border-none"
+              <img class="w-full h-65 object-cover object-center border-none"
                    :src="m.url" alt=""
                    :loading="index === 0 ? 'eager' : 'lazy'"/>
             </SplideSlide>
@@ -82,7 +82,15 @@ const closePopup = () => {
         </div>
       </template>
 
-      <div class="w-full text-warning-content/80 bg-warning/10 opacity-80 border-none select-none rounded-none py-3 px-2 font-semibold text-md text-start"
+      <div class="w-full h-12 relative"
+           v-else-if="!product!.badge?.length">
+        <div class="absolute w-full top-0 h-12 border-b-1 border-base-300 overflow-hidden flex flex-col justify-center">
+        <DiagonalPattern class="scale-165 opacity-60 text-warning-content/80"
+                         :establishment="establishment ?? 'restaurant'"/>
+      </div>
+    </div>
+
+      <div class="w-full text-warning-content/80 bg-warning/10 opacity-80 border-none select-none rounded-none py-3 px-2 pr-16 font-semibold text-md text-start"
            v-if="product!.badge?.length">
         {{ product.badge }}
       </div>
@@ -109,20 +117,7 @@ const closePopup = () => {
           </div>
         </div>
 
-        <div class="flex flex-col gap-1 px-6 py-1 bg-base-200/50">
-          <!-- Nutrition and timing info -->
-          <div class="flex gap-4" v-if="product.preparation_time || product.calories">
-            <div v-if="product.preparation_time" class="flex items-center gap-2">
-              <Timer class="w-5 h-5 text-gray-600" />
-              <span class="font-semibold">{{ i18n.t('badges.time', { minutes: product.preparation_time }) }}</span>
-            </div>
-
-            <div v-if="product.calories" class="flex items-center gap-2">
-              <Flame class="w-5 h-5 text-gray-600" />
-              <span class="font-semibold">{{ i18n.t('badges.calories', { calories: product.calories }) }}</span>
-            </div>
-          </div>
-
+        <div class="flex flex-col gap-1 px-6 py-1">
           <!-- Tags and Flags -->
           <div class="flex justify-between items-end gap-0">
             <div class="flex flex-wrap gap-x-3 gap-y-0.5 normal-case text-[14px] text-base-content/60 opacity-70">
@@ -238,37 +233,66 @@ const closePopup = () => {
           </div>
 
           <!-- Price and Variants -->
-          <div class="mb-4">
-            <h4 class="font-semibold text-lg mb-3">{{ i18n.t('Variants') }}</h4>
+          <div class="mb-4 mt-2">
+            <h4 class="font-semibold text-lg mb-3">{{ i18n.t('product.variants') }}</h4>
 
             <!-- Base price -->
-            <div class="flex justify-between items-center py-2 px-3 bg-base-200/50 rounded mb-2">
-              <div class="flex items-center gap-2">
-                <span class="font-medium">{{ product.weight }} {{ weightUnitFormatted(product.weight_unit ?? '') }}</span>
+            <div class="w-full flex flex-col justify-start items-center py-2 px-3 rounded mb-2 btn-outline border border-dashed text-base-content/75 border-base-content/40">
+              <div class="w-full flex justify-between items-center">
+                <div class="flex items-center gap-2">
+                  <span class="font-semibold text-lg">{{ product.weight }} {{ weightUnitFormatted(product.weight_unit ?? '') }}</span>
+                </div>
+                <span class="font-bold text-xl">{{ priceFormatted(product.price, currency?.toLowerCase() ?? 'uah') }}</span>
               </div>
-              <span class="font-bold text-xl">{{ priceFormatted(product.price, currency?.toLowerCase() ?? 'uah') }}</span>
+
+              <!-- Nutrition and timing info -->
+              <div class="w-full flex justify-start gap-4 opacity-80 text-[16px]" v-if="product.preparation_time || product.calories">
+                <div v-if="product.preparation_time" class="flex items-center gap-2">
+                  <Timer class="w-4 h-4 text-gray-600" />
+                  <span class="text-md">{{ i18n.t('badges.time', { minutes: product.preparation_time }) }}</span>
+                </div>
+
+                <div v-if="product.calories" class="flex items-center gap-2">
+                  <Flame class="w-4 h-4 text-gray-600" />
+                  <span class="text-md">{{ i18n.t('badges.calories', { calories: product.calories }) }}</span>
+                </div>
+              </div>
             </div>
 
             <!-- Variants -->
             <div v-if="product.variants?.length" class="space-y-2">
-              <div v-for="variant in product.variants" :key="variant.id"
-                   class="flex justify-between items-center py-2 px-3 bg-base-200/50 rounded">
-                <div class="flex items-center gap-2">
-                  <span class="font-medium">{{ variant.weight }} {{ weightUnitFormatted(variant.weight_unit ?? '') }}</span>
+              <div v-for="variant in product.variants" :key="variant.id" class="w-full flex flex-col justify-start items-center py-2 px-3 rounded mb-2 btn-outline border border-dashed text-base-content/75 border-base-content/40">
+                <div class="w-full flex justify-between items-center">
+                  <div class="flex items-center gap-2">
+                    <span class="font-semibold text-lg">{{ variant.weight }} {{ weightUnitFormatted(variant.weight_unit ?? '') }}</span>
+                  </div>
+                  <span class="font-bold text-xl">{{ priceFormatted(variant.price, currency?.toLowerCase() ?? 'uah') }}</span>
                 </div>
-                <span class="font-bold text-xl">{{ priceFormatted(variant.price, currency?.toLowerCase() ?? 'uah') }}</span>
+
+                <!-- Nutrition and timing info -->
+                <div class="w-full flex justify-start gap-4 opacity-80 text-[16px]" v-if="variant.preparation_time || variant.calories">
+                  <div v-if="variant.preparation_time" class="flex items-center gap-2">
+                    <Timer class="w-4 h-4 text-gray-600" />
+                    <span class="text-md">{{ i18n.t('badges.time', { minutes: variant.preparation_time }) }}</span>
+                  </div>
+
+                  <div v-if="variant.calories" class="flex items-center gap-2">
+                    <Flame class="w-4 h-4 text-gray-600" />
+                    <span class="text-md">{{ i18n.t('badges.calories', { calories: variant.calories }) }}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
         <!-- Content -->
-        <div class="px-4">
+        <div class="px-6">
           <!-- Tags and Flags -->
           <div class="mb-6">
             <!-- Allergens -->
             <div v-if="allergens.length > 0" class="mb-4">
-              <h4 class="font-semibold text-lg mb-2 text-orange-600">{{ i18n.t('badges.allergens') }}</h4>
+              <h4 class="font-semibold text-lg mb-2 text-orange-600/75">{{ i18n.t('badges.allergens') }}</h4>
               <div class="flex flex-wrap gap-2">
                 <div v-for="allergen in allergens" :key="allergen" class="flex items-center gap-1 text-orange-600/75 border border-dashed border-orange-600/75 px-2 py-1 rounded-sm">
                   <TriangleAlert class="w-4 h-4" />
